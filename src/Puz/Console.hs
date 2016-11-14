0427d43f118b25323e38b7cc1941a06cf7c5a48d
Jon Distad <jon.distad@gmail.com>
2016-11-13 19:24:02 -0500

    Top-level functions

--- src/Puz/Console.hs
+++ src/Puz/Console.hs
@@ -1,4 +1,4 @@
-module Puz.Console where
+module Puz.Console (console) where
 
 import Puz.Prelude
 import Puz.Printer
@@ -6,34 +6,34 @@ import Puz.Types
 import Puz.Util
 import System.IO
 
-prompt :: (MonadIO m) => m ()
-prompt = liftIO $ do
-  putStr "> "
-  hFlush stdout
-
 console :: (Game m) => m ()
 console = do
   prompt
   cmd <- liftIO getLine >>= return . map toLower
   dispatch cmd
+
+prompt :: (MonadIO m) => m ()
+prompt = liftIO $ do
+  putStr "> "
+  hFlush stdout
+
+dispatch :: (Game m) => String -> m ()
+dispatch "quit" = setMode Quit
+dispatch "exit" = setMode Quit
+dispatch "help" = do
+  liftIO . putStrLn . unlines $ [ "Commands:"
+                                , "  board - Display the board"
+                                , "  solution - Display the solution"
+                                , "  clues - Display the clues"
+                                , "  play - Play the crossword!"
+                                ]
+dispatch "board" = printPlayerBoard
+dispatch "solution" = printSolution
+dispatch "clues" = do
+  clues <- asks (clues :: Puzzle -> [Clue])
+  liftIO . putStrLn . unlines $ clueStrs clues
   where
-    dispatch :: (Game m) => String -> m ()
-    dispatch "quit" = setMode Quit
-    dispatch "exit" = setMode Quit
-    dispatch "help" = do
-      liftIO . putStrLn . unlines $ [ "Commands:"
-                                    , "  board - Display the board"
-                                    , "  solution - Display the solution"
-                                    , "  clues - Display the clues"
-                                    , "  play - Play the crossword!"
-                                    ]
-    dispatch "board" = printPlayerBoard
-    dispatch "solution" = printSolution
-    dispatch "clues" = do
-      clues <- asks (clues :: Puzzle -> [Clue])
-      liftIO . putStrLn . unlines $ clueStrs clues
-      where
-        clueStrs = map cs . sortBy (compare `on` ((,) <$> direction <*> number))
-        cs Clue{..} = show number ++ " " ++ show direction ++ ": " ++ text
-    dispatch "play" = setMode Play
-    dispatch s = liftIO (putStrLn $ s ++ "?? Wut?")
+    clueStrs = map cs . sortBy (compare `on` ((,) <$> direction <*> number))
+    cs Clue{..} = show number ++ " " ++ show direction ++ ": " ++ text
+dispatch "play" = setMode Play
+dispatch s = liftIO (putStrLn $ s ++ "?? Wut?")
--- src/Puz/Play.hs
+++ src/Puz/Play.hs
@@ -1,4 +1,4 @@
-module Puz.Play where
+module Puz.Play (play) where
 
 import Puz.Play.Movement
 import Puz.Play.Typing
@@ -25,37 +25,43 @@ play = do
       liftIO $ hFlush stdout
       c <- liftIO getChar
       dispatch c
-    dispatch (ctrl -> 'a') = beginningOfWord
-    dispatch (ctrl -> 'e') = endOfWord
-    dispatch (ctrl -> 'n') = moveDown
-    dispatch (ctrl -> 'p') = moveUp
-    dispatch (ctrl -> 'b') = moveLeft
-    dispatch (ctrl -> 'f') = moveRight
-    dispatch (ctrl -> 'd') = blankCurrentCell
-    dispatch (ctrl -> 'w') = jumpToClue
-    dispatch (ctrl -> 'y') = showErrors
-    dispatch ' ' = toggleDirection
-    dispatch c@(isLetter -> True) = fillCurrentCell (Just c)
-    dispatch '\DEL' = fillCurrentCell Nothing
-    dispatch '\ESC' = do
-      c <- liftIO getChar
+
+dispatch :: (Game m) => Char -> m ()
+dispatch (ctrl -> 'a') = beginningOfWord
+dispatch (ctrl -> 'e') = endOfWord
+dispatch (ctrl -> 'n') = moveDown
+dispatch (ctrl -> 'p') = moveUp
+dispatch (ctrl -> 'b') = moveLeft
+dispatch (ctrl -> 'f') = moveRight
+dispatch (ctrl -> 'd') = blankCurrentCell
+dispatch (ctrl -> 'w') = jumpToClue
+dispatch (ctrl -> 'y') = showErrors
+dispatch ' ' = toggleDirection
+dispatch c@(isLetter -> True) = fillCurrentCell (Just c)
+dispatch '\DEL' = fillCurrentCell Nothing
+dispatch '\ESC' = do
+  c <- liftIO getChar
+  case c of
+   'x' -> setupConsole >> setMode Console
+   '[' -> do
+     c' <- liftIO getChar
+     case c' of
+      'A' -> moveUp
+      'B' -> moveDown
+      'C' -> moveRight
+      'D' -> moveLeft
+      d@(isDigit -> True) -> getNum [d] >>= \n ->
+        case n of
+         "3" -> blankCurrentCell
+         _ -> return ()
+      _ -> return ()
+   _ -> return ()
+  where
+    getNum digits = liftIO getChar >>= \c ->
       case c of
-       'x' -> setupConsole >> setMode Console
-       '[' -> do
-         c' <- liftIO getChar
-         case c' of
-          'A' -> moveUp
-          'B' -> moveDown
-          'C' -> moveRight
-          'D' -> moveLeft
-          d@(isDigit -> True) -> getNum [d] >>= \n ->
-            case n of
-             "3" -> blankCurrentCell
-             _ -> return ()
-          _ -> return ()
-       _ -> return ()
-    dispatch _ = liftIO $ putStrLn "Nothing to do..."
-    ctrl c = chr (ord c + ord 'a' - 1)
-    getNum digits = liftIO getChar >>= \c -> case c of
-                                              '~' -> return (reverse digits)
-                                              n -> getNum (n:digits)
+       '~' -> return (reverse digits)
+       n -> getNum (n:digits)
+dispatch _ = liftIO $ putStrLn "Nothing to do..."
+
+ctrl :: Char -> Char
+ctrl c = chr (ord c + ord 'a' - 1)
